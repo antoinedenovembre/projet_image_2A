c634e518009fb56f6dd9ac7c2071c24ac66ab302
Antoine Duteyrat <aduteyrat@gmail.com>
2024-05-22 14:47:30 +0200

    Appli avec interop

---
+++ IHM/ProjetCPP/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace ProjetCPP
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            pictureBoxvterrain = new PictureBox();
+            pictureBoxTraite = new PictureBox();
+            pictureBoxiinitiale = new PictureBox();
+            btnStartStop = new Button();
+            btnOuvrir = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            labelVerdict = new Label();
+            timer = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBoxvterrain).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxTraite).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxiinitiale).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBoxvterrain
+            // 
+            pictureBoxvterrain.BorderStyle = BorderStyle.FixedSingle;
+            pictureBoxvterrain.Location = new Point(599, 113);
+            pictureBoxvterrain.Name = "pictureBoxvterrain";
+            pictureBoxvterrain.Size = new Size(285, 258);
+            pictureBoxvterrain.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxvterrain.TabIndex = 0;
+            pictureBoxvterrain.TabStop = false;
+            // 
+            // pictureBoxTraite
+            // 
+            pictureBoxTraite.BorderStyle = BorderStyle.FixedSingle;
+            pictureBoxTraite.Location = new Point(318, 113);
+            pictureBoxTraite.Name = "pictureBoxTraite";
+            pictureBoxTraite.Size = new Size(275, 258);
+            pictureBoxTraite.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxTraite.TabIndex = 1;
+            pictureBoxTraite.TabStop = false;
+            // 
+            // pictureBoxiinitiale
+            // 
+            pictureBoxiinitiale.BorderStyle = BorderStyle.FixedSingle;
+            pictureBoxiinitiale.Location = new Point(28, 113);
+            pictureBoxiinitiale.Name = "pictureBoxiinitiale";
+            pictureBoxiinitiale.Size = new Size(284, 258);
+            pictureBoxiinitiale.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxiinitiale.TabIndex = 2;
+            pictureBoxiinitiale.TabStop = false;
+            // 
+            // btnStartStop
+            // 
+            btnStartStop.Location = new Point(620, 400);
+            btnStartStop.Name = "btnStartStop";
+            btnStartStop.Size = new Size(236, 68);
+            btnStartStop.TabIndex = 4;
+            btnStartStop.Text = "Début/Pause cycle";
+            btnStartStop.UseVisualStyleBackColor = true;
+            btnStartStop.Click += startStopCycle;
+            // 
+            // btnOuvrir
+            // 
+            btnOuvrir.Location = new Point(333, 400);
+            btnOuvrir.Name = "btnOuvrir";
+            btnOuvrir.Size = new Size(245, 68);
+            btnOuvrir.TabIndex = 5;
+            btnOuvrir.Text = "Ouvrir Images";
+            btnOuvrir.UseVisualStyleBackColor = true;
+            btnOuvrir.Click += ouvrir;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(117, 25);
+            label1.TabIndex = 6;
+            label1.Text = "Image initiale";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(318, 65);
+            label2.Name = "label2";
+            label2.Size = new Size(116, 25);
+            label2.TabIndex = 7;
+            label2.Text = "Image traitée";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(599, 65);
+            label3.Name = "label3";
+            label3.Size = new Size(111, 25);
+            label3.TabIndex = 8;
+            label3.Text = "Vérité terrain";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(890, 65);
+            label4.Name = "label4";
+            label4.Size = new Size(66, 25);
+            label4.TabIndex = 9;
+            label4.Text = "Verdict";
+            // 
+            // labelVerdict
+            // 
+            labelVerdict.BackColor = Color.White;
+            labelVerdict.BorderStyle = BorderStyle.FixedSingle;
+            labelVerdict.Location = new Point(890, 113);
+            labelVerdict.Name = "labelVerdict";
+            labelVerdict.Size = new Size(266, 258);
+            labelVerdict.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1228, 515);
+            Controls.Add(labelVerdict);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btnOuvrir);
+            Controls.Add(btnStartStop);
+            Controls.Add(pictureBoxiinitiale);
+            Controls.Add(pictureBoxTraite);
+            Controls.Add(pictureBoxvterrain);
+            Name = "Form1";
+            Text = "ProjetCPP";
+            ((System.ComponentModel.ISupportInitialize)pictureBoxvterrain).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxTraite).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxiinitiale).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBoxvterrain;
+        private PictureBox pictureBoxTraite;
+        private PictureBox pictureBoxiinitiale;
+        private Button btnStartStop;
+        private Button btnOuvrir;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label labelVerdict;
+        private System.Windows.Forms.Timer timer;
+    }
+}
--- IHM/ProjetCPP/Form1.cs
+++ IHM/ProjetCPP/Form1.cs
@@ -1,11 +1,9 @@
-ï»¿using System;
+using libImage;
 using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Windows.Forms;
+using System.Drawing.Imaging;
+using Image = System.Drawing.Image;
 
-namespace IHM
+namespace ProjetCPP
 {
     public partial class Form1 : Form
     {
@@ -14,7 +12,7 @@ namespace IHM
         private string[] fichiersImageInit;
         private string[] fichiersVeriteTerrain;
 
-        // Indices des images actuellement affichÃ©es
+        // Indices des images actuellement affichées
         private int indexImageInit = 0;
         private int indexVeriteTerrain = 0;
 
@@ -27,7 +25,7 @@ namespace IHM
             timer.Interval = 200;
             timer.Tick += Timer_Tick;
         }
-     
+
         private void ouvrir(object sender, EventArgs e)
         {
             // Ouvrir un dialogue pour choisir un dossier contenant les deux dossier image_init_source et verite_terrain
@@ -46,21 +44,21 @@ namespace IHM
         {
             if (!cycleEnCours)
             {
-                // Ne pas initialiser les images ici pour continuer lÃ  oÃ¹ on s'est arrÃªtÃ©
+                // Ne pas initialiser les images ici pour continuer là où on s'est arrêté
                 timer.Start();
                 btnStartStop.Text = "Pause";
             }
             else
             {
                 timer.Stop();
-                btnStartStop.Text = "DÃ©but";
+                btnStartStop.Text = "Début";
             }
             cycleEnCours = !cycleEnCours;
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            // Mettre Ã  jour l'image initiale
+            // Mettre à jour l'image initiale
             if (indexImageInit < fichiersImageInit.Length)
             {
                 pictureBoxiinitiale.ImageLocation = fichiersImageInit[indexImageInit++];
@@ -70,7 +68,27 @@ namespace IHM
                 indexImageInit = 0;
             }
 
-            // Mettre Ã  jour l'image de vÃ©ritÃ© terrain
+            Bitmap bmp = new Bitmap(pictureBoxiinitiale.Image);
+            ClImage Img = new ClImage();
+
+            unsafe
+            {
+                BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+                Img.objetLibDataImgPtr(1, bmpData.Scan0, bmpData.Stride, bmp.Height, bmp.Width);
+                // 1 champ texte retour C++, le seuil auto
+                bmp.UnlockBits(bmpData);
+            }
+
+            int seuil = (int)Img.objetLibValeurChamp(0);
+
+            label4.Text = "Seuil auto : " + seuil;
+
+            displayVerdict();
+
+            // transférer C++ vers bmp
+            pictureBoxTraite.Image = bmp;
+
+            // Mettre à jour l'image de vérité terrain
             if (indexVeriteTerrain < fichiersVeriteTerrain.Length)
             {
                 pictureBoxvterrain.ImageLocation = fichiersVeriteTerrain[indexVeriteTerrain++];
@@ -88,14 +106,14 @@ namespace IHM
 
             if (Directory.Exists(dossierImageInit) && Directory.Exists(dossierVeriteTerrain))
             {
-                // Charger la premiÃ¨re image de image_init_source
+                // Charger la première image de image_init_source
                 fichiersImageInit = Directory.EnumerateFiles(dossierImageInit).Where(file => file.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase) || file.EndsWith(".png", StringComparison.OrdinalIgnoreCase)).ToArray();
                 if (fichiersImageInit.Length > 0)
                 {
                     pictureBoxiinitiale.ImageLocation = fichiersImageInit[0];
                 }
 
-                // Charger la premiÃ¨re image de verite_terrain
+                // Charger la première image de verite_terrain
                 fichiersVeriteTerrain = Directory.EnumerateFiles(dossierVeriteTerrain).Where(file => file.EndsWith(".bmp", StringComparison.OrdinalIgnoreCase) || file.EndsWith(".png", StringComparison.OrdinalIgnoreCase)).ToArray();
                 if (fichiersVeriteTerrain.Length > 0)
                 {
@@ -107,17 +125,17 @@ namespace IHM
                 MessageBox.Show("Le dossier choisi ne contient pas les dossiers image_init_source et verite_terrain", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
-            // RÃ©initialiser les indices
+            // Réinitialiser les indices
             indexImageInit = 0;
             indexVeriteTerrain = 0;
         }
 
-        private void displayVerdict(object sender, EventArgs e)
+        private void displayVerdict()
         {
-            // Supposons que la mÃ©thode CalculerPourcentage retourne un double entre 0 et 100
+            // Supposons que la méthode CalculerPourcentage retourne un double entre 0 et 100
             double pourcentage = CalculScore();
 
-            // DÃ©finir la couleur de fond du label Verdict basÃ© sur le pourcentage
+            // Définir la couleur de fond du label Verdict basé sur le pourcentage
             if (pourcentage > 90)
             {
                 labelVerdict.BackColor = Color.Green;
@@ -138,6 +156,7 @@ namespace IHM
             return 95.0;
         }
 
+        /*
         private void sensHoraireToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Image img = pictureBoxiinitiale.Image;
@@ -145,25 +164,26 @@ namespace IHM
             Bitmap bmp = new Bitmap(img);
             Bitmap bmp2 = new Bitmap(image);
 
-            // Rotation de 90 degrÃ©s dans le sens horaire
+            // Rotation de 90 degrés dans le sens horaire
             bmp.RotateFlip(RotateFlipType.Rotate270FlipNone);
             bmp2.RotateFlip(RotateFlipType.Rotate270FlipNone);
             pictureBoxiinitiale.Image = bmp;
             pictureBoxvterrain.Image = bmp2;
         }
 
-        private void sensTrigonomÃ©triqueToolStripMenuItem_Click(object sender, EventArgs e)
+        private void sensTrigonométriqueToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Image img = pictureBoxiinitiale.Image;
             Image image = pictureBoxvterrain.Image;
             Bitmap bmp = new Bitmap(img);
             Bitmap bmp2 = new Bitmap(image);
 
-            // Rotation de 90 degrÃ©s dans le sens antihoraire
+            // Rotation de 90 degrés dans le sens antihoraire
             bmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
             bmp2.RotateFlip(RotateFlipType.Rotate90FlipNone);
             pictureBoxiinitiale.Image = bmp;
             pictureBoxvterrain.Image = bmp2;
         }
+        */
     }
 }
